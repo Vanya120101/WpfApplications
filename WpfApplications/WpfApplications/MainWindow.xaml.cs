9a7f7748addef4639ee57aba4ec2cc8cbcda831d
Vanya <Vanya.120101@yandex.ru>
2022-01-30 18:22:13 +0300

    Finished project

--- WpfApplications/WpfApplications/MainWindow.xaml.cs
+++ WpfApplications/WpfApplications/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace WpfApplications
 		{
 			InitializeComponent();
 		}
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if(e.LeftButton == MouseButtonState.Pressed)
+			{
+				DragMove();
+			}
+		}
 	}
 }
